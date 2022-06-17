f9388ec58e2cc13824412b341197c0a6be30653f
Leo Chung <leo.chung97@gmail.com>
2022-06-17 16:51:52 -0400

    Root Reducer started - frontend auth to be completed

---
+++ frontend/actions/session.js
@@ -0,0 +1,13 @@
+import { postUser, postSession, deleteSession } from "../utils/session";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
