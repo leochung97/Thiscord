f616599cae17208fd56d410c2802efe3205c1a92
Leo Chung <leo.chung97@gmail.com>
2022-06-19 18:10:08 -0400

    Sessions Reducer completed

--- frontend/reducers/session.js
+++ frontend/reducers/session.js
@@ -4,3 +4,14 @@ const _nullSession = {
   currentUser: null
 }
 
+export default (state = _nullSession, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, { currentUser: action.user });
+    case LOGOUT_CURRENT_USER:
+      return _nullSession;
+    default:
+      return state;
+  }
+}
