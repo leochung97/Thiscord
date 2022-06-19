2a0ef831db4e32d03e2472bb87cefbb69cdaba68
Leo Chung <leo.chung97@gmail.com>
2022-06-19 19:39:49 -0400

    Updated App / Root for Signup Form Container - added components to prepare for NavBar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignupContainer from "./session/signup_container";
 
 const App = () => (
   <div>
-    <header>
-      <h1>Thiscord</h1>
-    </header>
+    <Routes>
+      <Route path="/signup" component={SignupContainer} />
+    </Routes>
   </div>
 );
 
