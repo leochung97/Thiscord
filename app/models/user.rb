521f1640fe51c36f8d38e752f80e7cb4e82b113b
Leo Chung <leo.chung97@gmail.com>
2022-06-20 15:17:33 -0400

    Added views for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   end
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
   end
 
   def reset_session_token!
--- frontend/reducers/session_errors.js
+++ frontend/reducers/session_errors.js
@@ -1,16 +1,16 @@
-import { RECEIVE_SESSION_ERRORS, CLEAR_SESSION_ERRORS } from "../actions/session";
+import { RECEIVE_SESSION_ERRORS, CLEAR_SESSION_ERRORS } from '../actions/session';
 
 const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
-  switch(action.type) {
+
+  switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
-      console.log(action);
-      return action;
+      return action.errors;
     case CLEAR_SESSION_ERRORS:
       return [];
     default:
       return state;
   }
-}
+};
 
 export default sessionErrorsReducer;
