31223043fa1ae12d2f7956a983a2a9c18a689708
Leo Chung <leo.chung97@gmail.com>
2022-06-21 17:10:55 -0400

    Header styling completed - need to add logo and background pictures

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -29,15 +29,16 @@ export default class Splash extends React.Component {
           <div className="splash-header-content">
             <h1>IMAGINE A PLACE...</h1>
             <h3>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</h3>
-          </div>
-          
-          <Link to="/login" className="splash-header-download-button">
-            Download for Mac
-          </Link>
+            <div className="splash-header-buttons">
+              <Link to="/login" className="splash-header-download-button">
+                Download for Mac
+              </Link>
 
-          <Link to="/login" className="splash-header-open-button">
-            Open Discord in your browser
-          </Link>
+              <Link to="/login" className="splash-header-open-button">
+                Open Discord in your browser
+              </Link>
+            </div>
+          </div>
         </header>
 
         <section>
