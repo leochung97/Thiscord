faa8ac10a7237a2ccfe391843fa5944f76ebb99d
Leo Chung <leo.chung97@gmail.com>
2022-06-29 22:09:11 -0400

    Reverted changes made to Channel Messages

--- frontend/components/app/servers/servers.jsx
+++ frontend/components/app/servers/servers.jsx
@@ -58,7 +58,7 @@ class Servers extends React.Component {
             }
             <div className="servers-separator"></div>
             <li className="modal-add-list" onClick={this.openModal}>
-              <svg viewBox="0 0 24 24">
+              <svg width="24" height="24" viewBox="0 0 24 24">
                 <path d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z"></path>
               </svg>
             </li>
