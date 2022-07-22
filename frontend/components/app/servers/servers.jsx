4a3ea895a5e6b28076ca778bb93189683ab7f704
Leo Chung <leo.chung97@gmail.com>
2022-07-22 17:48:01 -0400

    Attempted to fix server onClick

--- frontend/components/app/servers/servers.jsx
+++ frontend/components/app/servers/servers.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { logout } from "../../../actions/session_actions";
 import { fetchServers, createServer, deleteServer } from "../../../actions/server_actions";
 import ServerModal from "./server_modal";
@@ -11,8 +11,6 @@ class Servers extends React.Component {
     this.state = { IsOpen: false };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
-    this.handleRemoveServer = this.handleRemoveServer.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -32,16 +30,7 @@ class Servers extends React.Component {
   closeModal() {
     this.setState({ IsOpen: false });
   }
-
-  handleRemoveServer(serverId) {
-    this.props.deleteServer(serverId)
-  }
-
-  handleClick(serverId) {
-    let history = useHistory();
-    history.push(`/channels/${serverId}`)
-  }
-
+  
   render() {
     const { servers } = this.props;
 
@@ -57,7 +46,7 @@ class Servers extends React.Component {
             <div className="servers-separator"></div>
             {
               servers.map(server =>
-                <div className="servers-bubble" onClick={() => handleClick(server.id)}>
+                <div className="servers-bubble">
                   {server.server_name[0].toUpperCase()}
                 </div>
               )
@@ -74,7 +63,9 @@ class Servers extends React.Component {
             </li>
             <div className="servers-separator"></div>
             <li className="servers-logout-button" onClick={this.props.logout}>
-              <img src="https://thiscord-assets.s3.amazonaws.com/icons8-enter-30.png" />
+              <svg aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
+                <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z"></path>
+              </svg>
             </li>
           </ul>
           <ServerModal
