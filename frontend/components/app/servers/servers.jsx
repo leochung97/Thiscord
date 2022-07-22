a77ab4c1628c1f21256b2b5378f37d016aa82f99
Leo Chung <leo.chung97@gmail.com>
2022-07-22 18:32:27 -0400

    Added spacing to Servers.jsx to better highlight components

--- frontend/components/app/servers/servers.jsx
+++ frontend/components/app/servers/servers.jsx
@@ -47,7 +47,9 @@ class Servers extends React.Component {
                 </Link>
               </div>
             </li>
+            
             <div className="servers-separator"></div>
+            
             {
               servers.map(server =>
                 <div className="servers-bubble">
@@ -55,27 +57,34 @@ class Servers extends React.Component {
                 </div>
               )
             }
+
             <li className="modal-add-list" onClick={this.openModal}>
               <svg width="24" height="24" viewBox="0 0 24 24">
                 <path d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z"></path>
               </svg>
             </li>
+
             <li className="explore-public-servers">
               <svg width="24" height="24" viewbox="0 0 24 24">
                 <path d="M12 10.9C11.39 10.9 10.9 11.39 10.9 12C10.9 12.61 11.39 13.1 12 13.1C12.61 13.1 13.1 12.61 13.1 12C13.1 11.39 12.61 10.9 12 10.9ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM14.19 14.19L6 18L9.81 9.81L18 6L14.19 14.19Z"></path>
               </svg>
             </li>
+
             <div className="servers-separator"></div>
+
             <li className="servers-download-button" onClick={this.props.logout}>
               <svg aria-hidden="false" width="24" height="24" viewBox="0 0 24 24">
                 <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M16.293 9.293L17.707 10.707L12 16.414L6.29297 10.707L7.70697 9.293L11 12.586V2H13V12.586L16.293 9.293ZM18 20V18H20V20C20 21.102 19.104 22 18 22H6C4.896 22 4 21.102 4 20V18H6V20H18Z"></path>
               </svg>
             </li>
+
           </ul>
+          
           <ServerModal
             isOpen={this.state.IsOpen}
             closeModal={this.closeModal}
           />
+
         </div>
       </div>
     )
