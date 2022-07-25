6507b8ada439aebec147e2702ecc47835c0d6a55
Leo Chung <leo.chung97@gmail.com>
2022-07-25 11:37:46 -0400

    Revised Channels Modal to use React Hooks

--- frontend/components/app/channels/channel_modal.jsx
+++ frontend/components/app/channels/channel_modal.jsx
@@ -2,26 +2,20 @@ import React from "react";
 import Modal from "react-modal";
 import CreateChannel from "./create_channel";
 
-class ChannelModal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Modal
-        className="server-modal"
-        isOpen={this.props.isOpen}
-        onRequestClose={this.props.closeModal}
-        closetimeoutMS={50}
-        ariaHideApp={false}
-      >
-        <CreateChannel
-          closeModal={this.props.closeModal}
-        />
-      </Modal>
-    )
-  }
+function ChannelModal(props) {
+  return (
+    <Modal
+      className="server-modal"
+      isOpen={props.isOpen}
+      onRequestClose={props.closeModal}
+      closetimeoutMS={50}
+      ariaHideApp={false}
+    >
+      <CreateChannel
+        closeModal={props.closeModal}
+      />
+    </Modal>
+  )
 }
 
 export default ChannelModal;
