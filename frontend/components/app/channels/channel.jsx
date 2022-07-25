ed78e4e61e8b5abf7377e84047c8af9f6117a793
Leo Chung <leo.chung97@gmail.com>
2022-07-25 11:36:22 -0400

    Revised Channel.jsx to render /channels/@me if there is no longer a server with that id

--- frontend/components/app/channels/channel.jsx
+++ frontend/components/app/channels/channel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { useHistory, withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { fetchChannel, fetchChannels, createChannel, deleteChannel } from "../../../actions/channel_actions";
 import { deleteServer } from "../../../actions/server_actions";
@@ -10,6 +10,7 @@ import ServerEditModal from "./server_edit_modal"
 function Channels(props) {
   const [isOpen, setOpen] = useState(false);
   const [isEditOpen, setEditOpen] = useState(false);
+  const history = useHistory();
 
   const openModal = () => {
     setOpen(true);
@@ -26,57 +27,53 @@ function Channels(props) {
     setEditOpen(false);
   }
 
-  const removeChannel = channelId => {
-    props.deleteChannel(channelId);
-  };
-
-  const removeServer = serverId => {
-    props.deleteServer(serverId);
-  }
-
-  return (
-    <div className="sidebar-wrapper">
-      <h1>{ props.server.server_name }</h1>
-      <div className="channels-wrapper">
-        <div className="channels">
-          <div className="channels-drop-container">
-            <div className="channels-drop">TEXT CHANNELS</div>
-            <button className="channels-create-button" onClick={openModal}>+</button>
+  if (props.server) {
+    return (
+      <div className="sidebar-wrapper">
+        <h1>{ props.server.server_name }</h1>
+        <div className="channels-wrapper">
+          <div className="channels">
+            <div className="channels-drop-container">
+              <div className="channels-drop">TEXT CHANNELS</div>
+              <button className="channels-create-button" onClick={openModal}>+</button>
+            </div>
+            <ul>
+              {
+                props.server.channels.map(channel =>
+                  <div>
+                    <li className="channels-list" key={channel.id}>
+                      <img src="https://thiscord-assets.s3.amazonaws.com/icons8-hashtag-large-48.png" />
+                      <Link to={`/channels/${props.match.params.serverId}/${channel.id}`}>{channel.channel_name}</Link>
+                      <img src="https://thiscord-assets.s3.amazonaws.com/icons8-add-user-group-man-man-24.png" />
+                      <img src="https://thiscord-assets.s3.amazonaws.com/icons8-settings-32.png" />
+                    </li>
+                  </div>
+                )
+              }
+            </ul>
           </div>
-          <ul>
-            {
-              props.server.channels.map(channel =>
-                <div>
-                  <li className="channels-list" key={channel.id}>
-                    <img src="https://thiscord-assets.s3.amazonaws.com/icons8-hashtag-large-48.png" />
-                    <Link to={`/channels/${props.match.params.serverId}/${channel.id}`}>{channel.channel_name}</Link>
-                    <img src="https://thiscord-assets.s3.amazonaws.com/icons8-add-user-group-man-man-24.png" />
-                    <img src="https://thiscord-assets.s3.amazonaws.com/icons8-settings-32.png" />
-                  </li>
-                </div>
-              )
-            }
-          </ul>
+          <div className="sidebar-delete-server-container" onClick={openEdit}>
+            <div className="sidebar-delete-server">Edit Server</div>
+            <img src="https://thiscord-assets.s3.amazonaws.com/icons8-settings-32.png" />
+          </div>
         </div>
-        <div className="sidebar-delete-server-container" onClick={openEdit}>
-          <div className="sidebar-delete-server">Edit Server</div>
-          <img src="https://thiscord-assets.s3.amazonaws.com/icons8-settings-32.png" />
-        </div>
+
+        <ServerEditModal
+          isOpen={isEditOpen}
+          closeModal={closeModal}
+          server={props.server}
+        />
+
+        <ChannelModal
+          isOpen={isOpen}
+          closeModal={closeModal}
+        />
+
       </div>
-
-      <ServerEditModal
-        isOpen={isEditOpen}
-        closeModal={closeModal}
-        server={props.server}
-      />
-
-      <ChannelModal
-        isOpen={isOpen}
-        closeModal={closeModal}
-      />
-
-    </div>
-  )
+    )
+  } else {
+    history.push("/channels/@me")
+  }
 }
 
 const mSTP = (state, ownProps) => ({
