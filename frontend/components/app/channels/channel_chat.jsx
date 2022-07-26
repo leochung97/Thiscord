5de31997c6cc14aebdb845bb23466478233ef570
Leo Chung <leo.chung97@gmail.com>
2022-07-26 11:42:51 -0400

    Worked on action cable components

--- frontend/components/app/channels/channel_chat.jsx
+++ frontend/components/app/channels/channel_chat.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { createConsumer } from "@rails/actioncable"
 
 class ChannelChat extends React.Component {
   constructor(props) {
@@ -11,10 +12,14 @@ class ChannelChat extends React.Component {
   }
 
   componentDidMount() {
+    console.log("This is App Cable Subscriptions");
+    console.log(App.cable.subscriptions);
     App.cable.subscriptions.create(
-      { channel: "ChatChannel" },
+      { channel: this.props.currentChannelId },
       {
         received: data => {
+          console.log("This is the data")
+          console.log(data)
           switch (data.type) {
             case "message":
               this.setState({
@@ -53,7 +58,7 @@ class ChannelChat extends React.Component {
         <div className="message-list">
           {
             this.state.messages.map((message, idx) => 
-              <li key={message.id}>
+              <li key={idx}>
                 <div className="user-avatar-container">
                   <img src="https://thiscord-assets.s3.amazonaws.com/icon_clyde_white_RGB.svg" />
                 </div>
