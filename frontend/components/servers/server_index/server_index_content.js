9bf8f784407675c5aac6edf1651e6e87ace7aae5
Leo Chung <leo.chung97@gmail.com>
2022-08-15 15:56:58 -0400

    Added back ServerIndexContent

--- frontend/components/servers/server_index/server_index_content.js
+++ frontend/components/servers/server_index/server_index_content.js
@@ -2,19 +2,25 @@ import React from "react";
 import ServerIndexItem from "./server_index_item";
 
 const ServerIndexContent = (props) => {
-  return (
-    <ul className="server-index-modal-content">
-      {props.servers.map((server) => {
-        return (
-          <ServerIndexItem
-            server={server}
-            handleClick={props.handleClick}
-            key={server.id}
-          />
-        );
-      })}
-    </ul>
-  );
+  if (props.servers.length > 0) {
+    return (
+      <ul className="server-index-modal-content">
+        {props.servers.map((server) => {
+          return (
+            <ServerIndexItem
+              server={server}
+              handleClick={props.handleClick}
+              key={server.id}
+            />
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return (
+      <div className="no-server-text">No public servers left to join!</div>
+    );
+  }
 };
 
 export default ServerIndexContent;
