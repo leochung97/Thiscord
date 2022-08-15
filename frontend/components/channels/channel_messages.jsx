e8dbbf46296895460cf6563a128278b22b9e2fc3
Leo Chung <leo.chung97@gmail.com>
2022-08-15 18:29:55 -0400

    Added cable to everything

--- frontend/components/channels/channel_messages.jsx
+++ frontend/components/channels/channel_messages.jsx
@@ -17,7 +17,7 @@ const ChannelMessages = (props) => {
   useEffect(() => {
     props.fetchChannel(props.channel.id);
     // const cable = createConsumer("ws://localhost:3000/cable");
-    const cable = createConsumer("wss://thiscord-leo.herokuapp.com");
+    const cable = createConsumer("wss://thiscord-leo.herokuapp.com/cable");
 
     const paramsToSend = {
       channel: "ChannelChannel",
