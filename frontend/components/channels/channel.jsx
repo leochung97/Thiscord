d1c4dd5ed1f83e7647446a97e26a1d22a0dcffeb
Leo Chung <leo.chung97@gmail.com>
2022-08-15 16:31:03 -0400

    Added about page to channel header

--- frontend/components/channels/channel.jsx
+++ frontend/components/channels/channel.jsx
@@ -118,8 +118,10 @@ class Channel extends React.Component {
           <div className="channel-name">{this.props.channel.name}</div>
           <div className="space"></div>
 
-          <div data-tip data-for="tt-icons" className="channel-icons">
+          <div className="channel-icons">
             <svg
+              data-tip
+              data-for="tt-icons"
               x="0"
               y="0"
               aria-hidden="false"
@@ -139,6 +141,8 @@ class Channel extends React.Component {
             </svg>
 
             <svg
+              data-tip
+              data-for="tt-icons"
               x="0"
               y="0"
               aria-hidden="false"
@@ -164,7 +168,13 @@ class Channel extends React.Component {
               ></path>
             </svg>
 
-            <svg width="24" height="24" viewBox="0 0 24 24">
+            <svg
+              data-tip
+              data-for="tt-icons"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
               <path
                 fill="currentColor"
                 d="M22 12L12.101 2.10101L10.686 3.51401L12.101 4.92901L7.15096 9.87801V9.88001L5.73596 8.46501L4.32196 9.88001L8.56496 14.122L2.90796 19.778L4.32196 21.192L9.97896 15.536L14.222 19.778L15.636 18.364L14.222 16.95L19.171 12H19.172L20.586 13.414L22 12Z"
@@ -172,6 +182,8 @@ class Channel extends React.Component {
             </svg>
 
             <svg
+              data-tip
+              data-for="tt-icons"
               x="0"
               y="0"
               aria-hidden="false"
@@ -201,7 +213,7 @@ class Channel extends React.Component {
               ></path>
             </svg>
 
-            <div className="channel-search-bar">
+            <div data-tip data-for="tt-icons" className="channel-search-bar">
               <span>Search</span>
               <svg
                 aria-hidden="false"
@@ -230,7 +242,14 @@ class Channel extends React.Component {
                 fill="currentColor"
               ></path>
             </svg>
+          </div>
 
+          <a
+            className="about-link"
+            href="https://github.com/leochung97/Thiscord"
+            data-tip
+            data-for="tt-about"
+          >
             <svg
               x="0"
               y="0"
@@ -244,7 +263,7 @@ class Channel extends React.Component {
                 d="M12 2C6.486 2 2 6.487 2 12C2 17.515 6.486 22 12 22C17.514 22 22 17.515 22 12C22 6.487 17.514 2 12 2ZM12 18.25C11.31 18.25 10.75 17.691 10.75 17C10.75 16.31 11.31 15.75 12 15.75C12.69 15.75 13.25 16.31 13.25 17C13.25 17.691 12.69 18.25 12 18.25ZM13 13.875V15H11V12H12C13.104 12 14 11.103 14 10C14 8.896 13.104 8 12 8C10.896 8 10 8.896 10 10H8C8 7.795 9.795 6 12 6C14.205 6 16 7.795 16 10C16 11.861 14.723 13.429 13 13.875Z"
               ></path>
             </svg>
-          </div>
+          </a>
         </div>
         <ReactTooltip
           id="tt-icons"
@@ -253,7 +272,16 @@ class Channel extends React.Component {
           effect="solid"
           backgroundColor="#202225"
         >
-          <span>Icons to be updated!</span>
+          <span>To be updated!</span>
+        </ReactTooltip>
+        <ReactTooltip
+          id="tt-about"
+          place="bottom"
+          type="dark"
+          effect="solid"
+          backgroundColor="#202225"
+        >
+          <span>Project GitHub</span>
         </ReactTooltip>
 
         <div className="channel-content-shell">
