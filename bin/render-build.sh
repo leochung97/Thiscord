bef806b104d9b2d6b87e4e9fe2158e8c0c54aaa8
Leo Chung <leo.chung97@gmail.com>
2022-11-29 18:11:38 -0500

    Using different render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,10 +3,9 @@
 set -o errexit
 
 bundle install
+npm install
+npm run build
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-
-#if site renders but no seeded data is present add this line 
-
 rails db:seed #if needed, comment this line out whenever pushing to a working deployment to avoid db errors
