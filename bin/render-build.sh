42f330374bf2514cb3b9eaf37437304b39cf49ce
Leo Chung <leo.chung97@gmail.com>
2022-11-29 18:10:42 -0500

    Change render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+#if site renders but no seeded data is present add this line 
+
+rails db:seed #if needed, comment this line out whenever pushing to a working deployment to avoid db errors
