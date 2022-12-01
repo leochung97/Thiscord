94a7e20d224edac8171bb29e66fe6ce5918820c7
Leo Chung <leo.chung97@gmail.com>
2022-11-30 19:28:37 -0500

    Removed ability to scroll on x-axis

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -11,7 +11,7 @@ class Api::SessionsController < ApplicationController
       login!(@user)
       render "api/users/show"
     else 
-      render json: [" - Login or password is invalid"], status: 401
+      render json: ["  - Login or password is invalid"], status: 401
     end
   end
   
